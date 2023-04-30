ed72090a98f059b5d57be98af2d350a8a88e0f47
ZAIN-KHAN-25 <zainarfeen87@gmail.com>
2023-04-30 13:08:33 +0500

    masla solve hogya pehla wala

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
 var images = [{
     serialnumber: '1',
-    srcofimg : './1jpg',
+    srcofimg : './1.jpg',
     idofelement : 'one',
 },
 {
     serialnumber: '2',
-    srcofimg : "./2jpeg",
+    srcofimg : "./2.jpeg",
     idofelement: "two",
 },{
     serialnumber: '3',
-    srcofimg : "./3jpeg",
+    srcofimg : "./3.jpeg",
     idofelement: "three",
 },{
     serialnumber: '4',
-    srcofimg : "./4jpeg",
+    srcofimg : "./4.jpeg",
     idofelement: "four",
 },{
     serialnumber: '5',
-    srcofimg : "./5jpeg",
+    srcofimg : "./5.jpeg",
     idofelement: "five",
 },{
     serialnumber: '6',
-    srcofimg : "./6jpeg",
+    srcofimg : "./6.jpeg",
     idofelement: "six",
 }];
 
@@ -38,8 +38,23 @@ var cindex = 0;
 var totalimageslength = images.length;
 // console.log(totalimageslength);
 
-for(let i = 0; i < images.length; i++) {
-  frame.innerHTML = `<img src='${images[i].srcofimg}' title='${images[i].serialnumber}' alt='${images[i].idofelement}' id='${images[i].idofelement}' class="img">`; 
-    
-  console.log(`<img src="${images[i].srcofimg}" title="${images[i].serialnumber}" alt="" id="${images[i].idofelement}" class="img">`)
-}
+async function loadImages() {
+    for (let i = 0; i < images.length; i++) {
+      var imgcreation = document.createElement('img');
+      await imgcreation.setAttribute('src', images[i].srcofimg);
+      await imgcreation.setAttribute('title', images[i].serialnumber);
+      await imgcreation.setAttribute('alt', images[i].idofelement);
+      await imgcreation.setAttribute('id', images[i].idofelement);
+      await imgcreation.setAttribute('class', 'img');
+      
+    await frame.appendChild(imgcreation);
+      
+      console.log(`<img src="${images[i].srcofimg}" title="${images[i].serialnumber}" alt="" id="${images[i].idofelement}" class="img">`)
+    }
+  }
+  
+  loadImages();
+
+  
+  
+  
